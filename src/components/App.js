3326d739e46f85825c7ce89597b0c129c39f2ee9
Harshita222 <kumariharsita501@gmail.com>
2024-07-04 13:36:56 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,23 @@ import "./../styles/App.css";
 const App = () => {
   const [product, setProduct] = useState([]);
 
-  const productApi = async () => {
-    const response = await fetch("https://dummyjson.com/products");
-    const res = await response.json();
-    setProduct(res.products);
-    console.log(res);
-  };
-
   useEffect(() => {
-    productApi();
-  }, []);
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/products");
+        const data = await response.json();
+        setProducts(data.products);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
+    fetchData();
+  }, []);
+  
   return (
     <div>
-      <h1>Data Fetched from Api</h1>
+      <h1>Data Fetched from API</h1>
       <pre>{JSON.stringify(product, null, 2)}</pre>
     
     </div>
