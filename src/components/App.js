18aa671a0b908d7b468a4490656a239fbb416434
Harshita222 <kumariharsita501@gmail.com>
2024-07-04 13:07:30 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [product, setProduct] = useState([]);
+
+  const productApi = async () => {
+    const response = await fetch("https://dummyjson.com/products");
+    const res = await response.json();
+    setProduct(res.products);
+    console.log(res);
+  };
+
+  useEffect(() => {
+    productApi();
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Data Fetched from Api</h1>
+      <pre>{JSON.stringify(product, null, 2)}</pre>
+    
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
